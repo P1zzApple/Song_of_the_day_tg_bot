5d334662995202529b4d8a32a22b3370e9026d9d
P1zzApple <96815420+P1zzApple@users.noreply.github.com>
2024-09-24 16:38:55 +0500

    Create config.py
    First commit

---
+++ config.py
@@ -0,0 +1,17 @@
+# db connect
+host = "localhost"
+user = "postgres"
+password = "password"
+db_name = "db_name"
+port = 1234
+
+# tg user id
+log = 1234
+
+# spotify oauth
+bot_token = 'tg_bot_token'
+sp_client_id = 'spotify_client_id'
+sp_client_secret = 'spotify_client_secret'
+sp_username = 'spotify_username'
+# to be removed
+uri1, uri2, uri3 = 'random_uri'
